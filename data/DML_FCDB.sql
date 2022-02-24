95b78b4b5863fd5bd500a0c8870cb28b6d938fc5
Lauren Ruff <48524322+LaurenMRuff@users.noreply.github.com>
2022-02-24 10:41:09 -0800

    formatting changes, added missing semicolon, commented out DELETE for Jobs, Customers, Categories, and Employees, reordered job_employee search table

--- data/DML_FCDB.sql
+++ data/DML_FCDB.sql
@@ -4,12 +4,13 @@
 -- Query for add a new character functionality with colon : character being used to
 -- denote the variables that will have data from the backend programming language
 
--- Jobs DML-- 
+-- ------------------Jobs DML------------------
 
 -- Jobs -- SELECT --
 -- Get all information to populate Jobs table page
 SELECT *
-FROM Jobs;
+FROM Jobs
+ORDER BY job_id;
 
 -- Get category names to populate Dropdown list within "Add new Job"
 SELECT category_name
@@ -23,93 +24,101 @@ SELECT CONCAT(
                 customer_first_name,
                 ' ',
                 customer_last_name
-        ) as id_name
+        ) AS id_name
 FROM Customers
 ORDER BY customer_id;
 
 -- Jobs -- INSERT --
 INSERT INTO Jobs (
-                customer_id,
-                category_id,
-                job_code,
-                job_start_date,
-                job_end_date,
-                job_description,
-                job_status
-        )
+                  customer_id,
+                  category_id,
+                  job_code,
+                  job_start_date,
+                  job_end_date,
+                  job_description,
+                  job_status
+                  )
 VALUES(
-                :customer_id,
-                :category_id,
-                :job_code,
-                :job_start_date,
-                :job_end_date,
-                :job_description,
-                :job_status
-        );
+                  :customer_id,
+                  :category_id,
+                  :job_code,
+                  :job_start_date,
+                  :job_end_date,
+                  :job_description,
+                  :job_status
+                  );
 
 -- Jobs -- UPDATE --
 UPDATE Jobs
 SET customer_id = :customer_id,
-        category_id = :category_id,
-        job_code = :job_code,
-        job_start_date = :job_start_date,
-        job_end_date = :job_end_date,
-        job_description = :job_description,
-        job_status = :job_status
+    category_id = :category_id,
+    job_code = :job_code,
+    job_start_date = :job_start_date,
+    job_end_date = :job_end_date,
+    job_description = :job_description,
+    job_status = :job_status
 WHERE job_id = :job_id;
 
 -- Jobs -- DELETE --
-DELETE FROM Jobs
-WHERE job_id = :job_id;
+# DELETE FROM Jobs
+# WHERE job_id = :job_id;
 
--- Customers DML-- 
+
+--
+-- ------------------Customers DML------------------
+--
+
 -- Customers -- SELECT --
 -- Get all data to populate data tables within Customers Page
 SELECT *
-FROM Customers;
+FROM Customers
+order by customer_id;
 
 -- Customers -- INSERT --
 INSERT INTO Customers(
-                customer_first_name,
-                customer_last_name,
-                customer_email,
-                customer_phone,
-                customer_address1,
-                customer_city,
-                customer_state,
-                customer_zip_code,
-                customer_company
-        )
+                      customer_first_name,
+                      customer_last_name,
+                      customer_email,
+                      customer_phone,
+                      customer_address1,
+                      customer_city,
+                      customer_state,
+                      customer_zip_code,
+                      customer_company
+                      )
 VALUES(
-                :customer_first_name,
-                :customer_last_name,
-                :customer_email,
-                :customer_phone,
-                :customer_address1,
-                :customer_city,
-                :customer_state,
-                :customer_zip_code,
-                :customer_company
-        );
+                      :customer_first_name,
+                      :customer_last_name,
+                      :customer_email,
+                      :customer_phone,
+                      :customer_address1,
+                      :customer_city,
+                      :customer_state,
+                      :customer_zip_code,
+                      :customer_company
+                      );
 
 -- Customers -- UPDATE --
 UPDATE Customers
 SET customer_first_name = :customer_first_name,
-        customer_last_name = :customer_last_name,
-        customer_email = :customer_email,
-        customer_phone = :customer_phone,
-        customer_address1 = :customer_address,
-        customer_city = :customer_city,
-        customer_state = :customer_state,
-        customer_zip_code = :customer_zip_code,
-        customer_company = :customer_company
+    customer_last_name = :customer_last_name,
+    customer_email = :customer_email,
+    customer_phone = :customer_phone,
+    customer_address1 = :customer_address,
+    customer_city = :customer_city,
+    customer_state = :customer_state,
+    customer_zip_code = :customer_zip_code,
+    customer_company = :customer_company
 WHERE customer_id = :customer_id;
 
 -- Customers -- DELETE --
-DELETE FROM Customers
-WHERE customer_id = :customer_id;
+# DELETE FROM Customers
+# WHERE customer_id = :customer_id;
 
--- Categories DML-- 
+--
+-- ------------------Categories DML------------------
+--
+
 -- Categories -- SELECT --
 SELECT *
 FROM Categories
@@ -125,57 +134,58 @@ SET category_name = :category_name
 WHERE category_id = :category_id;
 
 -- Categories -- DELETE --
-DELETE FROM Categories
-WHERE category_id = :category_id;
+# DELETE FROM Categories
+# WHERE category_id = :category_id;
 
--- Employees DML-- 
+--
+-- ------------------Employees DML------------------
+--
+
 -- Employees -- SELECT --
 -- Get all data to populate data tables within Employees Page
 SELECT *
-FROM Employees;
+FROM Employees
+ORDER BY employee_id;
 
 -- Employees -- INSERT --
 INSERT INTO Employees (
-                employee_code,
-                employee_first_name,
-                employee_last_name,
-                employee_email,
-                employee_job_title
-        )
-VALUES(
-                :employee_code,
-                :employee_first_name,
-                :employee_last_name,
-                :employee_email,
-                :employee_job_title
-        );
+                       employee_code,
+                       employee_first_name,
+                       employee_last_name,
+                       employee_email,
+                       employee_job_title
+                       )
+VALUES(                :employee_code,
+                       :employee_first_name,
+                       :employee_last_name,
+                       :employee_email,
+                       :employee_job_title
+                       );
 
 -- Employees -- UPDATE --
 UPDATE Employees
 SET employee_code = :employee_code,
-        employee_first_name = :employee_first_name,
-        employee_last_name = :employee_last_name,
-        employee_email = :employee_email,
-        employee_job_title = :employee_job_title
+    employee_first_name = :employee_first_name,
+    employee_last_name = :employee_last_name,
+    employee_email = :employee_email,
+    employee_job_title = :employee_job_title
 WHERE employee_id = :employee_id;
 
 -- Employees -- DELETE --
-DELETE FROM Employees
-WHERE employee_id = :employee_id;
+# DELETE FROM Employees
+# WHERE employee_id = :employee_id;
 
--- Job_Employees DML-- 
+--
+-- ------------------Job_Employees DML------------------
+--
+
 -- Job_Employees -- SELECT --
 -- Get all data to populate data tables within Job_Employees Page
 SELECT job_employee_id,
-        job_id,
-        employee_id
+       job_id,
+       employee_id
 FROM Job_Employees
 ORDER BY job_employee_id;
-
--- Get category names to populate Dropdown list within "Add new Job"
-SELECT category_name
-FROM Categories
-ORDER BY category_id;
 
 -- Get Existing Employee ID-names to populate Dropdown list within "Add a New Job_Employee Relationship"
 SELECT CONCAT(
@@ -199,49 +209,54 @@ ORDER BY job_id;
 
 -- Job_Employees -- INSERT --
 INSERT INTO Job_Employee(job_id, employee_id)
-VALUES(job_id = :job_id, employee_id = :employee_id);
+VALUES(job_id = :job_id,
+       employee_id = :employee_id);
 
 -- Job_Employees -- UPDATE --
 UPDATE Job_Employees
 SET employee_id = :employee_id,
-        job_id = :job_id
+    job_id = :job_id
 WHERE job_employee_id = :job_employee_id;
 
 -- Job_Employees -- DELETE --
 DELETE FROM Job_Employees
-WHERE job_employee_id = :job_employee_id -- DML for Job_Employee_Search SELECT
+WHERE job_employee_id = :job_employee_id;
 
+--
+-- ------------------Job_Employee Relationship DML------------------
+--
 
--- Job_Employee Relationship DML-- 
+-- DML for Job_Employee_Search SELECT
 -- Display available data to populate data tables within Job_Employees_Search.HTML landing Page
-SELECT job_employee_id,
+SELECT je.job_employee_id,
         e.employee_id,
+        CONCAT(e.employee_first_name, ' ', e.employee_last_name) AS name,
+        e.employee_job_title AS title,
         j.job_id,
-        CONCAT(employee_first_name, ' ', employee_last_name) AS name,
-        employee_job_title AS title,
-        job_description,
+        e.job_description,
         c.category_id,
         c.category_name
 FROM Employees AS e
-        INNER JOIN Job_Employees AS je on e.employee_id = je.employee_id
+        INNER JOIN Job_Employees AS je ON e.employee_id = je.employee_id
         INNER JOIN Jobs AS j ON j.job_id = je.job_id
-        INNER JOIN Categories AS c on c.category_id = j.category_id
-ORDER BY job_employee_id;
+        INNER JOIN Categories AS c ON c.category_id = j.category_id
+ORDER BY je.job_employee_id;
 
 -- Display Employees working on specific jobs based on Job_ID selection
-SELECT job_employee_id,
+SELECT je.job_employee_id,
         e.employee_id,
+        CONCAT(e.employee_first_name, ' ', e.employee_last_name) AS name,
+        e.employee_job_title AS title,
         j.job_id,
-        CONCAT(employee_first_name, ' ', employee_last_name) AS name,
-        employee_job_title AS title,
-        job_description,
+        j.job_description,
         c.category_id,
         c.category_name
 FROM Employees AS e
-        INNER JOIN Job_Employees AS je on e.employee_id = je.employee_id
+        INNER JOIN Job_Employees AS je ON e.employee_id = je.employee_id
         INNER JOIN Jobs AS j ON j.job_id = je.job_id
-        INNER JOIN Categories AS c on c.category_id = j.category_id
-WHERE j.job_id =:selected_job_id;
+        INNER JOIN Categories AS c ON c.category_id = j.category_id
+WHERE j.job_id =:selected_job_id
+ORDER BY je.job_employee_id;
 
 -- Get Job ID-description to populate Dropdown list within "Search for Job"
 SELECT CONCAT(
