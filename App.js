56a51830b976d0b0667089086a37fabd1ba81d8f
Lauren Ruff <48524322+LaurenMRuff@users.noreply.github.com>
2022-03-03 11:08:08 -0800

    All READ and CREATE are functional

--- App.js
+++ App.js
@@ -21,15 +21,13 @@ hbs.registerPartials(__dirname + '/partials');
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 
-/*
-    ROUTES
-*/
-// WORKS!
+// ---------- ROUTES ----------
+// WORKS - Home Page
 app.get('/', function (req, res) {
     res.render('index', { title: "Home Page", active: { Register: true } });
 });
 
-// WORKS!
+// WORKS - READ Jobs
 app.get('/jobs', function (req, res) {
     // dropdown populating lists
     let customers = [];
@@ -37,9 +35,9 @@ app.get('/jobs', function (req, res) {
     let jobStatus = ['New', 'In Progress', 'Complete', 'Abandoned'];
 
     // queries
-    let queryAllJobs = "SELECT * FROM Jobs ORDER BY job_id;";
-    let queryCustomerID = "SELECT customer_id FROM Customers ORDER BY customer_id";
-    let queryCategoryID = "SELECT category_id FROM Categories ORDER BY category_id";
+    let queryAllJobs = `SELECT * FROM Jobs ORDER BY job_id;`;
+    let queryCustomerID = `SELECT customer_id FROM Customers ORDER BY customer_id`;
+    let queryCategoryID = `SELECT category_id FROM Categories ORDER BY category_id`;
 
     db.pool.query(queryCustomerID, function (err, rows, fields) {
         for (let i = 0; i < rows.length; i++) {
@@ -60,7 +58,7 @@ app.get('/jobs', function (req, res) {
     })
 });
 
-// WORKS!
+// WORKS - CREATE/INSERT Jobs
 app.post('/add-job', function (req, res) {
     let data = req.body;
 
@@ -80,11 +78,11 @@ app.post('/add-job', function (req, res) {
         job_status) 
     
     VALUES (
-        ${data['customer_ID_Select']}, 
-        ${data['category_ID_Select']}, 
+        '${data['customer_ID_Select']}', 
+        '${data['category_ID_Select']}', 
         '${data['job_code']}', 
         '${data['job_start_date']}', 
-        ${end_date}, 
+        '${end_date}', 
         '${data['job_description']}', 
         '${data['job_status']}')`;
 
@@ -106,7 +104,7 @@ app.post('/add-job', function (req, res) {
     })
 });
 
-// WORKS! WORKING KIARA
+// WORKS - READ Customers
 app.get('/customers', function (req, res) {
     // dropdown populating lists
 
@@ -115,7 +113,7 @@ app.get('/customers', function (req, res) {
     'MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','PR',
     'RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY'];
 
-    let queryAllCustomers = "SELECT * FROM Customers ORDER BY customer_id;";
+    let queryAllCustomers = `SELECT * FROM Customers ORDER BY customer_id;`;
 
 
     db.pool.query(queryAllCustomers, function (err, rows, fields) {
@@ -125,9 +123,15 @@ app.get('/customers', function (req, res) {
     })
 });
 
-// NOT WORKING:(-- 3/3/2022
+// WORKS - CREATE/INSERT Customers
 app.post('/add-customer', function (req, res) {
     let data = req.body;
+
+    let customer_company = data.customer_company;
+    if(isNaN(customer_company) || customer_company === 'undefined')
+    {
+        customer_company = null;
+    }
 
     let queryAddCustomer =
     `INSERT INTO Customers (
@@ -135,22 +139,22 @@ app.post('/add-customer', function (req, res) {
         customer_last_name,
         customer_email,
         customer_phone,
-        customer_address1,
+        customer_company,
+        customer_address,
         customer_city,
         customer_state,
-        customer_zip_code,
-        customer_company)
+        customer_zip_code)
 
     VALUES (
     '${data['customer_first_name']}',
     '${data['customer_last_name']}',
     '${data['customer_email']}',
     '${data['customer_phone']}',
-    '${data['customer_address1']}',
+    '${customer_company}',
+    '${data['customer_address']}',
     '${data['customer_city']}',
-    ${data['customer_state_Select']},
-    '${data['customer_zip_code']}',
-    '${data['customer_company']}')`;
+    '${data['customer_state_Select']}',
+    '${data['customer_zip_code']}')`;
 
     db.pool.query(queryAddCustomer, function (error, rows, fields) {
 
@@ -170,8 +174,7 @@ app.post('/add-customer', function (req, res) {
     })
 });
 
-
-// WORKS!
+// WORKS - READ Categories
 app.get('/categories', function (req, res) {
     //NEED TO ADD DROPDOWN FORM INFO
 
@@ -182,8 +185,7 @@ app.get('/categories', function (req, res) {
     })
 });
 
-
-// WORKING!! -- 3/3/2022
+// WORKS - CREATE/INSERT Categories
 app.post('/add-category', function (req, res) {
     let data = req.body;
 
@@ -210,9 +212,7 @@ app.post('/add-category', function (req, res) {
     })
 });
 
-
-
-// WORKS! -- 3/3/2022 With Dropdown populating lists
+// WORKS - READ Employees
 app.get('/employees', function (req, res) {
     // dropdown populating lists
 
@@ -229,23 +229,23 @@ app.get('/employees', function (req, res) {
 
 });
 
-//  WORKING :)-- 3/3/2022
+//  WORKS - CREATE/INSERT Employee
 app.post('/add-employee', function (req, res) {
     let data = req.body;
 
     let queryAddEmployee =
         `INSERT INTO Employees (
+        employee_code,
         employee_first_name,
         employee_last_name,
-        employee_code,
         employee_email,
         employee_job_title
         )
         
-    VALUES(                
+    VALUES(
+        '${data['employee_code']}',
         '${data['employee_first_name']}',
         '${data['employee_last_name']}',
-        '${data['employee_code']}',
         '${data['employee_email']}',
         '${data['employee_title']}')`;
 
@@ -268,17 +268,65 @@ app.post('/add-employee', function (req, res) {
     })
 });
 
-
-
-// WORKS!
+// WORKS - READ Job_Employees
 app.get('/job_employees', function (req, res) {
-    //NEED TO ADD DROPDOWN FORM INFO
+
+    let employee_id = [];
+    let job_id = [];
+
+    let queryEmployeeID = `SELECT employee_id FROM Employees ORDER BY employee_id`;
+    let queryJobID = `SELECT category_id FROM Jobs ORDER BY category_id`;
+
+    db.pool.query(queryEmployeeID, function (err, rows, fields) {
+        for (let i = 0; i < rows.length; i++) {
+            employee_id.push(rows[i]["employee_id"]);
+        }
+    })
+
+    db.pool.query(queryJobID, function (err, rows, fields) {
+        for (let i = 0; i < rows.length; i++) {
+            job_id.push(rows[i]["job_id"]);
+        }
+    })
+
 
     //SELECTS ALL JOB_EMPLOYEES FROM JOB_EMPLOYEES TABLE AND ADDS THEM TO HTML TABLE
     let queryJobEmployees = "SELECT * FROM Job_Employees ORDER BY job_employee_id;";
     db.pool.query(queryJobEmployees, function (err, rows, fields) {
-        res.render('job_employees', { title: "Job_Employees Page", active: { Register: true }, data: rows }
+        res.render('job_employees', { title: "Job_Employees Page", active: { Register: true }, data: rows,
+            employee_id : employee_id, job_id : job_id}
         );
+    })
+});
+
+//  WORKS - CREATE/INSERT Job_Employee
+app.post('/add-job-employee', function (req, res) {
+    let data = req.body;
+
+    let queryAddJobEmployee =
+        `INSERT INTO Job_Employees (
+        fk_employee_id,
+        fk_job_id)
+    VALUES(
+        ${data['employee_id']},
+        ${data['job_id']})`;
+
+
+    db.pool.query(queryAddJobEmployee, function (error, rows, fields) {
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal, so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+
+            // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
+        // presents it on the screen
+        else {
+            res.redirect('/employees');
+        }
     })
 });
 
@@ -287,7 +335,7 @@ app.get('/job_employees_search', function (req, res) {
 
     let job_id = []
 
-    let queryJobID = "SELECT job_id FROM Jobs;";
+    let queryJobID = `SELECT job_id FROM Jobs;`;
 
     db.pool.query(queryJobID, function (err, rows, fields) {
         for (let i = 0; i < rows.length; i++) {
@@ -295,10 +343,22 @@ app.get('/job_employees_search', function (req, res) {
         }
     })
 
-    let queryJobEmployeesSearch = "SELECT Job_Employees.job_employee_id, Job_Employees.fk_job_id AS job_id, Jobs.job_description, Job_Employees.fk_employee_id AS employee_id, CONCAT(Employees.employee_first_name, ' ', Employees.employee_last_name) AS name, Employees.employee_job_title AS title, Categories.category_id, Categories.category_name FROM Employees INNER JOIN Job_Employees ON Employees.employee_id = Job_Employees.fk_employee_id INNER JOIN Jobs ON Jobs.job_id = Job_Employees.fk_job_id INNER JOIN Categories ON Categories.category_id = Jobs.category_id ORDER BY Job_Employees.job_employee_id;";
+    let queryJobEmployeesSearch =
+        `SELECT Job_Employees.job_employee_id AS job_employee_id, 
+                Jobs.job_id AS job_id, 
+                Jobs.job_description AS job_description, 
+                Job_Employees.fk_employee_id AS employee_id, 
+                CONCAT(Employees.employee_first_name, ' ', Employees.employee_last_name) AS name, 
+                Employees.employee_job_title AS title, 
+                Categories.category_id AS category_id, 
+                Categories.category_name AS category_name 
+        FROM Employees 
+            INNER JOIN Job_Employees ON Employees.employee_id = Job_Employees.fk_employee_id 
+            INNER JOIN Jobs ON Jobs.job_id = Job_Employees.fk_job_id 
+            INNER JOIN Categories ON Categories.category_id = Jobs.category_id 
+        ORDER BY Job_Employees.job_employee_id;`;
     
     db.pool.query(queryJobEmployeesSearch, function (err, rows, fields) {
-        //console.log(rows);
         res.render('job_employees_search', { title: "Job_Employees Search Page", active: { Register: true }, data: rows, job_id: job_id });
     })
 });
